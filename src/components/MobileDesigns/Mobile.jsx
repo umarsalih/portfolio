a995295ac154fbc5b874813e1c73e2e17b1d0fee
umarsalih <salihumar13@gmail.com>
2023-02-16 09:16:06 -0800

    add github link to mobile project. edit content on mobile page

--- src/components/MobileDesigns/Mobile.jsx
+++ src/components/MobileDesigns/Mobile.jsx
@@ -15,16 +15,28 @@ function Mobile() {
                     <Row>
                         <Col className='mobile-project-text' lg="6" xs="12">
                             <h2 className='underline'>NoMoSolo</h2>
-                            <p>A full-stack mobile app created in a group of four for the Mobile App
-                                Development course.</p>
-                            <p>This is a social media app where users can sign up to create a profile, list
-                                any musical talents they have, and meet with other musicians who want to create
-                                music with them</p>
+                            <p className='underline bold reduce-title-space'>Tech stack:</p>
+                            <p >
+
+                                figma, android studio, java, SQLite</p>
+                            <p className='justify'>this is a social media app where musicians can sign up to create a profile,
+                                list any musical talents they have, and meet with other musicians who want to
+                                create music with them. Some cool features include chat, notes, and viewing
+                                other registered users.</p>
+
                             <p>
-                                <p className='underline bold'>Tech stack:</p>
-                                Android Studio, Java, SQLite</p>
-                            <p>Contribution: Theme selection, design of logo, UI design of login, register,
-                                calendar, and chat</p>
+                                <b>Contribution: 
+                                </b> Theme selection, design of logo, UI design of login, register, calendar, and
+                                chat</p>
+
+                            <p className='underline bold reduce-title-space'>Link to project:
+                            </p>
+                            <a
+                                className='link-text fancy-link'
+                                target="_blank"
+                                href='https://github.com/bill071194/NoMoSolo'>
+                                Github
+                            </a>
                         </Col>
                         <Col className='mobile-project-carousel' lg="6" xs="12">
                             <CarouselProject/>
